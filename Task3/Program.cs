903238e9e3b832a3ee69912f30ee7b2f47ec1096
Karmazinanastya <karmazina133@gmail.com>
2023-09-12 23:35:49 +0300

    Initial commit

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Is bear sleeping? Press 'y' if he is.");
+        char userInput = Console.ReadKey().KeyChar;
+
+        if (userInput == 'y' || userInput == 'Y')
+        {
+            Console.WriteLine("\n───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───\r" +
+                              "\n───█▒▒░░░░░░░░░▒▒█───\r" +
+                              "\n────█░░▒░░░░░▒░░█────\r" +
+                              "\n─▄▄──█░░░▀█▀░░░█──▄▄─\r" +
+                              "\n█░░█─▀▄░░░░░░░▄▀─█░░█\r\n");
+        }
+        else
+        {
+            Console.WriteLine("\n───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───\r" +
+                              "\n───█▒▒░░░░░░░░░▒▒█───\r" +
+                              "\n────█░░█░░░░░█░░█────\r" +
+                              "\n─▄▄──█░░░▀█▀░░░█──▄▄─\r" +
+                              "\n█░░█─▀▄░░░░░░░▄▀─█░░█\r\n");
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть кількість повторювань: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("───▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄───\r" +
+                            "\n───█▒▒░░░░░░░░░▒▒█───\r" +
+                            "\n────█░░█░░░░░█░░█────\r" +
+                            "\n─▄▄──█░░░▀█▀░░░█──▄▄─\r" +
+                            "\n█░░█─▀▄░░░░░░░▄▀─█░░█\r\n");
+        }
+    }
+}
